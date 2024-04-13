5a03ea22aae5a0c062deae9680ea3080b8a17393
JuggerN4uti <97904924+JuggerN4uti@users.noreply.github.com>
2024-04-13 15:33:39 +0200

    przywoływanie żywiołaków, usuwanie i inne + 2 pierwsze żywiołaki

---
+++ Summoning2k24/Assets/Elemental.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Elemental : MonoBehaviour
+{
+    public PlayerController Player;
+    public int elementalID;
+    public float duration;
+    float maxDuration;
+    public Image DurationImage;
+
+    void Start()
+    {
+        maxDuration = duration;
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent(typeof(PlayerController)) as PlayerController;
+    }
+
+    void Update()
+    {
+        duration -= Time.deltaTime;
+        DurationImage.fillAmount = duration / maxDuration;
+        if (duration <= 0)
+            Perish();
+    }
+
+    void Perish()
+    {
+        Player.SummonsAviable[elementalID]++;
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.transform.tag == "Death")
+            Perish();
+        if (other.transform.tag == "Perish")
+            Perish();
+    }
+}
--- Summoning2k24/Assets/PlayerController.cs
+++ Summoning2k24/Assets/PlayerController.cs
@@ -5,9 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontal, speed, jumpStrength;
+    [Header("Movement")]
+    public float horizontal;
+    public float speed, jumpStrength;
     public bool facingRight;
 
+    [Header("Summon")]
+    public Vector3 mousePos;
+    public Transform CursosPosition;
+
+    [Header("Elementals")]
+    public int Summon;
+    public int[] SummonsAviable;
+    bool destroyCooldown;
+    public GameObject[] ElementalPrefabs;
+    public GameObject Destroyer;
+
+    [Header("SerializeFields")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
@@ -15,6 +29,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetMouseButtonDown(0))
+            SummonElemental();
+        if (Input.GetMouseButtonDown(1))
+            DestroyElemental();
+
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+            Summon = 0;
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+            Summon = 1;
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
@@ -54,5 +78,36 @@ public class PlayerController : MonoBehaviour
     {
         if (other.transform.tag == "Death")
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+
+    // Summoning
+    void SummonElemental()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CursosPosition.position = new Vector2(mousePos[0], mousePos[1]);
+
+        if (SummonsAviable[Summon] > 0)
+        {
+            Instantiate(ElementalPrefabs[Summon], CursosPosition.position, CursosPosition.rotation);
+            SummonsAviable[Summon]--;
+        }
+    }
+
+    void DestroyElemental()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CursosPosition.position = new Vector2(mousePos[0], mousePos[1]);
+
+        if (!destroyCooldown)
+        {
+            Instantiate(Destroyer, CursosPosition.position, CursosPosition.rotation);
+            destroyCooldown = true;
+            Invoke("OffCooldown", 0.5f);
+        }
+    }
+
+    void OffCooldown()
+    {
+        destroyCooldown = false;
     }
 }
